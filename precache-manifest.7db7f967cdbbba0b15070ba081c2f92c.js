06b716d352356de4c73dd9b4c63408965a88ff6f
sobia <sobiashahbaz66@yahoo.com>
2019-01-12 19:19:06 +0100

    Updates

---
+++ precache-manifest.7db7f967cdbbba0b15070ba081c2f92c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f99dc5b96220213d6122",
+    "url": "/e-commerce-fullstack-app/static/js/runtime~main.f99dc5b9.js"
+  },
+  {
+    "revision": "ba526bd810ca19473d3d",
+    "url": "/e-commerce-fullstack-app/static/js/main.ba526bd8.chunk.js"
+  },
+  {
+    "revision": "27093548551b230aee57",
+    "url": "/e-commerce-fullstack-app/static/js/1.27093548.chunk.js"
+  },
+  {
+    "revision": "ba526bd810ca19473d3d",
+    "url": "/e-commerce-fullstack-app/static/css/main.f143415c.chunk.css"
+  },
+  {
+    "revision": "0e26892861ad478ebfce6e248d9f1d6c",
+    "url": "/e-commerce-fullstack-app/index.html"
+  }
+];
