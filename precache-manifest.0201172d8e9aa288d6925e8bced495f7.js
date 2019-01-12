4724d2ff5df18a8c66517932f3b9a36f8ec69764
sobia <sobiashahbaz66@yahoo.com>
2019-01-12 23:45:42 +0100

    Updates

---
+++ precache-manifest.0201172d8e9aa288d6925e8bced495f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53fca9659b75b362b45a",
+    "url": "/e-commerce-fullstack-app/register_login/static/js/runtime~main.53fca965.js"
+  },
+  {
+    "revision": "0b2e9bc10d4d436cd419",
+    "url": "/e-commerce-fullstack-app/register_login/static/js/main.0b2e9bc1.chunk.js"
+  },
+  {
+    "revision": "27093548551b230aee57",
+    "url": "/e-commerce-fullstack-app/register_login/static/js/1.27093548.chunk.js"
+  },
+  {
+    "revision": "0b2e9bc10d4d436cd419",
+    "url": "/e-commerce-fullstack-app/register_login/static/css/main.f143415c.chunk.css"
+  },
+  {
+    "revision": "250224dedb709d2bfb54d31f1214c81a",
+    "url": "/e-commerce-fullstack-app/register_login/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/e-commerce-fullstack-app//register_login/precache-manifest.2966a4621ada5d66f07f8876cd00f3c4.js"
+  "/e-commerce-fullstack-app/register_login/precache-manifest.0201172d8e9aa288d6925e8bced495f7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/e-commerce-fullstack-app//register_login/index.html", {
+workbox.routing.registerNavigationRoute("/e-commerce-fullstack-app/register_login/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
