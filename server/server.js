9723e7f9ed287b33880f9002dedf472d2479e0e3
sobia <sobiashahbaz66@yahoo.com>
2019-02-15 23:08:27 +0100

    server changes for heroku

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useCreateIndex: true
 });
@@ -20,6 +20,7 @@ mongoose.connection.on("error", err => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(express.static("client/build"));
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -404,6 +405,13 @@ app.post("/api/site/site_data", auth, admin, (req, res) => {
     }
   );
 });
+
+if (process.env.NODE_ENV === "Production") {
+  const path = require("path");
+  app.get("/*", (req, res) => {
+    res.sendfile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 3002;
 
