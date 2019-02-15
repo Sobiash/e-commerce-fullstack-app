d373bd1fcd6c7a9f5ed1958e4456cf94696231e4
sobia <sobiashahbaz66@yahoo.com>
2019-02-15 23:48:51 +0100

    heroku settings

--- client/src/components/utils/config.js
+++ client/src/components/utils/config.js
@@ -1,5 +1,5 @@
 const USER_SERVER = "/api/users";
-const PRODUCT_SERVER = "/api/product/";
-const SITE_SERVER = "/api/site/";
+const PRODUCT_SERVER = "/api/product";
+const SITE_SERVER = "/api/site";
 
 export { USER_SERVER, PRODUCT_SERVER, SITE_SERVER };
