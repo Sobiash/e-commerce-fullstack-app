5c247418f9c1a609a3d3fd1a4ca80194fc8fa3fa
sobia <sobiashahbaz66@yahoo.com>
2019-02-16 02:10:44 +0100

    package.json/server changed

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ mongoose.connection.on("error", err => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static("client/build"));
+// app.use(express.static("client/build"));
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
