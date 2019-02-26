f5f047fb1b9aa19372a826e745e06bff4ebf56ab
sobia <sobiashahbaz66@yahoo.com>
2019-02-26 22:45:26 +0100

    heroku settings

--- server/server.js
+++ server/server.js
@@ -22,7 +22,9 @@ mongoose.connection.on("error", err => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "client", "build", "static")));
+app.use(
+  express.static(path.join(__dirname, "client", "build", "static", "js"))
+);
 // app.use(express.static(path.join(__dirname, "client", "public")));
 
 cloudinary.config({
