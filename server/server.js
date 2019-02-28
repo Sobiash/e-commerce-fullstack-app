376d3772f85455d048d39314f9bdbc439bd5c1ec
sobia <sobiashahbaz66@yahoo.com>
2019-02-28 11:58:49 +0100

    heroku

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ app.use(cookieParser());
 app.use(
   express.static(path.join(__dirname, "client", "build", "static", "js"))
 );
+app.use(express.static(path.join(__dirname, ".", "client", "public")));
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
