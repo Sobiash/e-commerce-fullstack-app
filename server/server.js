5643dd65bc99a41827a884b0b1e62fde56905af1
sobia <sobiashahbaz66@yahoo.com>
2019-03-02 11:07:02 +0100

    heroku

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ mongoose.connection.on("error", err => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "client", "build", "static")));
 app.use(express.static(path.join(__dirname, "client", "public")));
 
 cloudinary.config({
