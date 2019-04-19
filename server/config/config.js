18306e68d320af7ad96fd89a7f0dc8945c0fa3f8
sobia <sobiashahbaz66@yahoo.com>
2019-04-20 01:11:28 +0200

    heroku

---
+++ server/config/config.js
@@ -0,0 +1,34 @@
+const env = {
+  isDev: process.env.NODE_ENV == "development",
+  isTest: process.env.NODE_ENV == "test",
+  isProd: process.env.NODE_ENV == "production"
+};
+
+if (env.isProd) requiredEnvs(["TOKEN_SECRET", "MONGO_URI"]);
+
+const mongoUri = !env.isProd
+  ? "mongodb://localhost:27017/e-commerce-fullstack-app"
+  : process.env.MONGO_URI;
+
+const tokenSecret = !env.isProd
+  ? "SUPERSECRETPASSWORD123"
+  : process.env.TOKEN_SECRET;
+
+function requiredEnvs(envsArr) {
+  envsArr.forEach(env => {
+    if (!process.env[env]) {
+      throw `missing enviroment variable ${env}`;
+    }
+  });
+}
+
+module.exports = {
+  mongoConf: {
+    uri: mongoUri
+  },
+  expressConf: {
+    port: process.env.PORT || 3002
+  },
+  tokenSecret,
+  env
+};
