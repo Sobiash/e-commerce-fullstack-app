5d70b98b309a2275635a1cefef8a39504f3d1e46
sobia <sobiashahbaz66@yahoo.com>
2019-05-12 23:37:44 +0200

    .

--- server.js
+++ server.js
@@ -49,17 +49,25 @@ if (app.get("env") === "production") {
   });
 }
 
-(async function() {
-  try {
-    for (let route in routes) {
-      logger.info(`Attaching route: ${route}`);
-      // app.use(routes[route]);
-      app.use("/", routes[route]);
-    }
+// (async function() {
+//   try {
+//     for (let route in routes) {
+//       logger.info(`Attaching route: ${route}`);
+//       app.use(routes[route]);
+//       // app.use("/", routes[route]);
+//     }
 
-    const listener = app.listen(expressConf.port);
-    logger.info(`listening on port ${listener.address().port}`);
-  } catch (error) {
-    throw new Error(error);
-  }
-})();
+//     const listener = app.listen(expressConf.port);
+//     logger.info(`listening on port ${listener.address().port}`);
+//   } catch (error) {
+//     throw new Error(error);
+//   }
+// })();
+
+for (let route in routes) {
+  logger.info(`Attaching route: ${route}`);
+  app.use(routes[route]);
+  // app.use("/", routes[route]);
+}
+const listener = app.listen(expressConf.port);
+logger.info(`listening on port ${listener.address().port}`);
